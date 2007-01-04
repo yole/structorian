b46f2550555219bc35e67155fc1c9e6f7e6b46dd
intelliyole <intelliyole@7767d458-9e19-0410-b7be-41930ee49dfe>
2007-01-04 20:16:00 +0000

    generified function registry

---
+++ trunk/Structorian.Engine/FunctionRegistry.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Structorian.Engine
+{
+    class FunctionRegistry<T> where T : IEvaluateContext
+    {
+        protected delegate IConvertible FunctionDelegate(T context, Expression[] parameters);
+
+        private Dictionary<String, FunctionDelegate> _functions = new Dictionary<string, FunctionDelegate>(StringComparer.InvariantCultureIgnoreCase);
+
+        protected void Register(string name, FunctionDelegate functionDelegate)
+        {
+            _functions[name] = functionDelegate;
+        }
+
+        public IConvertible Evaluate(string function, Expression[] parameters, IEvaluateContext context)
+        {
+            FunctionDelegate evalDelegate;
+            if (!_functions.TryGetValue(function, out evalDelegate))
+                return null;
+            if (!(context is T))
+                throw new Exception("Invalid context type");
+
+            return evalDelegate((T) context, parameters);
+        }
+    }
+}
