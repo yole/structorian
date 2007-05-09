390cd9a9e43c483bb9e96506d7efc1bdddfb7ba0
intelliyole <intelliyole@7767d458-9e19-0410-b7be-41930ee49dfe>
2007-05-09 07:55:03 +0000

    initial support for zlib encoded blobs

--- Structorian.Engine/AttributeRegistry.cs
+++ Structorian.Engine/AttributeRegistry.cs
@@ -16,6 +16,7 @@ namespace Structorian.Engine
             RegisterAttribute(typeof(AssertField), "expr", AttributeType.Expression);
             RegisterAttribute(typeof(BitfieldField), "size", AttributeType.Expression);
             RegisterAttribute(typeof(BlobField), "len", AttributeType.Expression);
+            RegisterAttribute(typeof(BlobField), "encoding", AttributeType.String);
             RegisterAttribute(typeof(CalcField), "value", AttributeType.Expression);
             RegisterAttribute(typeof(CaseField), "expr", AttributeType.Expression);
             RegisterAttribute(typeof(CaseField), "default", AttributeType.Bool);
--- Structorian.Engine/Fields/BlobField.cs
+++ Structorian.Engine/Fields/BlobField.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using System.Text;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
 
 namespace Structorian.Engine.Fields
 {
@@ -17,6 +19,20 @@ namespace Structorian.Engine.Fields
             if (offset + len > reader.BaseStream.Length)
                 throw new LoadDataException("Blob size " + len + " exceeds stream length");
             byte[] blobBytes = reader.ReadBytes(len);
+            string encoding = GetStringAttribute("encoding");
+            if (encoding == "zlib")
+            {
+                InflaterInputStream stream = new InflaterInputStream(new MemoryStream(blobBytes));
+                byte[] data = new byte[4096];
+                MemoryStream outStream = new MemoryStream();
+                int size;
+                while((size = stream.Read(data, 0, data.Length)) > 0)
+                {
+                    outStream.Write(data, 0, size);
+                }
+                outStream.Capacity = (int) outStream.Length;
+                blobBytes = outStream.GetBuffer();
+            }
             BlobCell cell = new BlobCell(this, blobBytes, offset);
             instance.AddCell(cell, _hidden);
             instance.RegisterCellSize(cell, len);
