b1d1a59d282d432a5139d65619098493ff0485d1
intelliyole <intelliyole@7767d458-9e19-0410-b7be-41930ee49dfe>
2008-12-29 22:44:36 +0000

    bugfixes

--- Structorian.Engine/GroupContainer.cs
+++ Structorian.Engine/GroupContainer.cs
@@ -6,9 +6,9 @@ namespace Structorian.Engine
 {
     class GroupContainer: InstanceTreeNode
     {
-        private string _groupName;
+        private readonly string _groupName;
         private readonly InstanceTreeNode _parent;
-        private List<InstanceTreeNode> _children = new List<InstanceTreeNode>();
+        private readonly List<InstanceTreeNode> _children = new List<InstanceTreeNode>();
         private bool _notifyChildren = false;
 
         public GroupContainer(InstanceTreeNode parent, string groupName)
@@ -38,12 +38,15 @@ namespace Structorian.Engine
 
         public override void NeedChildren()
         {
-            InstanceTree tree = GetInstanceTree();
-            foreach(InstanceTreeNode child in _children)
+            if (!_notifyChildren)
             {
-                tree.NotifyInstanceAdded(this, child);
+                InstanceTree tree = GetInstanceTree();
+                foreach (InstanceTreeNode child in _children)
+                {
+                    tree.NotifyInstanceAdded(this, child);
+                }
+                _notifyChildren = true;
             }
-            _notifyChildren = true;
         }
 
         public override ReadOnlyCollection<StructCell> Cells
@@ -55,7 +58,6 @@ namespace Structorian.Engine
         {
             get { return _children[_children.Count - 1].EndChildrenOffset; }
         }
-
 
         public override StructInstance LastChild
         {
