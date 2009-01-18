06d7c2eeeca03d5cbd85db29331a9fcbc8fbc682
intelliyole <intelliyole@7767d458-9e19-0410-b7be-41930ee49dfe>
2009-01-18 19:45:10 +0000

    'while' field implemented

---
+++ trunk/Structorian.Engine/Fields/WhileField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Structorian.Engine.Fields
+{
+    class WhileField: StructField
+    {
+        public WhileField(StructDef structDef) : base(structDef, "expr", true)
+        {
+        }
+
+        public override void LoadData(BinaryReader reader, StructInstance instance)
+        {
+            while (GetExpressionAttribute("expr").EvaluateBool(instance))
+            {
+                try
+                {
+                    LoadChildFields(reader, instance);
+                }
+                catch (BreakRepeatException)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
